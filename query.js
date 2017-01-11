c9209503fb3193976515f64d34a3a4e52503b61e
nohmapp <nohman20@gmail.com>
2017-01-10 18:23:10 -0800

    mor etesting

--- query.js
+++ query.js
@@ -1,7 +1,19 @@
 const got = require('got');
 const fs = require('fs');
 
-got.stream("https:\//api.npms.io/v2/package/react").pipe(fs.createWriteStream('index.html'));
+//search suggestions
+//I really like the warning lfags that this gives
+//got.stream("https:\//api.npms.io/v2/search/suggestions?q=react + d3").pipe(fs.createWriteStream('data.json'));
+
+//search
+//got.stream("https:\//api.npms.io/v2/search?q=react + d3").pipe(fs.createWriteStream('data.json'));
+
+//package get, allows me to retrieve a specific package, I would use search
+//if I could always retrieve the right module
+got.stream("https:\//api.npms.io/v2/package/optimist").pipe(fs.createWriteStream('data.json'));
 
 //lets create an idea of what this should all return and look at and then
 //write the stream, and build out the first level of the visualization
+
+
+//When the time comes use htis to add a CLI element https://github.com/yargs/yargs
