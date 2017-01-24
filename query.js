16f5d3b8d7c35d07bf63ffeabd967abb98507960
nohmapp <nohman20@gmail.com>
2017-01-23 17:55:52 -0800

    makes api request and filters results

--- query.js
+++ query.js
@@ -1,17 +1,23 @@
 const fs = require('fs');
 const Promise = require("bluebird");
 const request = require('request-promise');
-const parse = require('./parse');
 
+var parsePkgJSON = function (){
+   return new Promise((resolve, reject) => {
+     fs.readFile('./package.json', 'utf-8', (err, data) => {
+       if (err) reject(err)
+       let contents = JSON.parse(data);//try and catch all the JSON parse, reject(new Error('OH SHiT'))
+       let packages = Object.keys(contents['dependencies']).concat(Object.keys(contents['devDependencies']));
+       resolve(packages)
+   });
+  });
+}
 
+var npmSearchQuery = function(requests){
+Promise.map(requests, request.get, {concurrency: 4}).then(function(apiResults) {
+  let filteredNPM = pkgInfoParse(apiResults)
 
-var query = function(requests){
-Promise.map(requests, request.get, {concurrency: 4}).then(function(results) {
-
-  //fs.writeFile('here.json', results)
-  var data1 = pkgInfoParse(results)
-  console.log(data1)
-  fs.writeFile('new.json', data1, (err) => {
+  fs.writeFile('data.json', filteredNPM, (err) => {
     if (err) throw err;
     console.log('done')
   })
@@ -21,37 +27,16 @@ Promise.map(requests, request.get, {concurrency: 4}).then(function(results) {
 }
 
 
-parse().then((packages) => {
+parsePkgJSON().then((packages) => {
   let packageUrls = packages.map((name) => {
     return "https://api.npms.io/v2/package/" + name
   })
-    //console.log(packageUrls)
-    query(packageUrls) //.pipe(fs.createWriteStream('neo.json'))
-
+    npmSearchQuery(packageUrls)
   })
-/*
-remove all weird characters, next try to stringify and then parse the returned
-object
-  s = s.replace(/\\n/g, "\\n")
-                 .replace(/\\'/g, "\\'")
-                 .replace(/\\"/g, '\\"')
-                 .replace(/\\&/g, "\\&")
-                 .replace(/\\r/g, "\\r")
-                 .replace(/\\t/g, "\\t")
-                 .replace(/\\b/g, "\\b")
-                 .replace(/\\f/g, "\\f");
-  // remove non-printable and other non-valid JSON chars
-  s = s.replace(/[\u0000-\u0019]+/g,"");
-  var o = JSON.parse(s);
-*/
-
 
 var pkgInfoParse = function(pkgInfo){
-//make this loop through each part and parse(maybe)
-//then grab a list of all the headlines I want
-//check with matt about error handling
-//later optimize this
-let parsedPkg ={}
+/*
+Use in case I abstract parse from the loop and need to pull out odd characters
 pkgInfo = pkgInfo.replace(/\\n/g, "\\n")
                  .replace(/\\'/g, "\\'")
                  .replace(/\\"/g, '\\"')
@@ -60,34 +45,24 @@ pkgInfo = pkgInfo.replace(/\\n/g, "\\n")
                  .replace(/\\t/g, "\\t")
                  .replace(/\\b/g, "\\b")
                  .replace(/\\f/g, "\\f");
-
 pkgInfo = pkgInfo.replace(/[\u0000-\u0019]+/g,"");
+*/
+  let filteredInfo = []
 
-  try {parsedPkg =
-      JSON.parse(pkgInfo)
-  } catch(e){
-   console.log(e)
-  }
+  pkgInfo.forEach((pkg) =>{
+    let parsedPkg = {}
+    let filteredPkg = {}
 
-  let filteredInfo = {}
+    try {parsedPkg =
+        JSON.parse(pkg)
+    } catch(e){
+     console.log(e)
+    }
 
-  //for (let pkg in parsedPkg){
-  parsedPkg.forEach((pkg) =>{
-
-    filteredInfo['score'] = pkg.score //in the case of undefined
-    filteredInfo['evaluation'] = pkg.evaluation
-    console.log(typeof pkg)
-        console.log(pkg.collected)
-
-    //fs.writeFile("test.json", pkg + '-------------------------------------')
-    return filteredInfo
+    filteredPkg['score'] = parsedPkg.score //add try/catch for more complex calls
+    filteredPkg['evaluation'] = parsedPkg.evaluation
+    filteredInfo.push(filteredPkg)
+    console.log(filteredInfo)
   })
+  return filteredInfo
 }
-    //lets grab dependencis and pkg info
-    //we'll parse first and append to new object the parts we want
-    //with error handling for undefined
-
-
-
-
-//query(["https://api.npms.io/v2/package/d3", "https://api.npms.io/v2/package/react"])
