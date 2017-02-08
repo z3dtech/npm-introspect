c0db6c553f7c9a352207e42e08b9e48d5e07057b
nohmapp <nohman20@gmail.com>
2017-02-08 14:28:08 -0800

    about to revert

--- client/index.js
+++ client/index.js
@@ -1,1 +1,55 @@
+
+
+// d3.json('http://localhost:8080/', function(err, json){
+//   if (err) console.log(err)
+
+window.onload = function (){
+
 const d3 = require('d3')
+  console.log('loaded')
+const height = window.innerHeight;
+const width = window.innerWidth;
+
+const svg = d3.select("body").append("svg")
+  .attrs({
+    width: width,
+    height: height
+  });
+
+const nodes = svg.selectAll('nodes')
+  .data(json)
+  .enter()
+  .append('circle')
+  .attrs({
+    cx: (width/2),
+    cy: (height/2),
+    r: 20
+  })
+
+const axis = d3.axisLeft() //scale
+
+svg.append('g')
+ .attr('class', 'axis')
+ .each((d) => {})
+
+}
+/*
+// Add an axis and title.
+g.append("g")
+  .attr("class", "axis")
+  .each(function(d) { d3.select(this).call(axis.scale(y[d])); })
+.append("text")
+  .style("text-anchor", "middle")
+  .attr("y", -9)
+  .text(function(d) { return d; });
+
+  // Extract the list of dimensions and create a scale for each.
+    x.domain(dimensions = d3.keys(cars[0]).filter(function(d) {
+      return d != "name" && (y[d] = d3.scale.linear()
+          .domain(d3.extent(cars, function(p) { return +p[d]; }))
+          .range([height, 0]));
+    }));
+
+});
+*/
+}
--- server/server.js
+++ server/server.js
@@ -5,14 +5,28 @@
 const requestData = require('../server/requestData')
 const express = require('express')
 const app = express()
+const path = require('path')
+const morgan = require('morgan')
 
 module.exports.go = () => {
 
+  //app.set('views', path.join(__dirname, 'v'));
+
+  app.use(morgan('combined'))
+
+
+  //app.use(express.static('../client/index.html'))
 
   app.get('/', function (req, res) {
+      res.sendFile(path.join(__dirname + '/../client/index.html'))
+      //app.use('/js', express.static(path.join(__dirname + '/client/index.js')))
       requestData.parse()
       .then(function (data) {
+          res.json(data)
           res.setHeader('Content-Type', 'text/plain');
+
+          //res.render(path.join(__dirname + '/../client/index.html'))
+
           res.end(data);
       })
       .catch(function (e) {
