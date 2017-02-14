32f2b57301228ea1a756ce079bdcad72548767c7
nohmapp <nohman20@gmail.com>
2017-02-13 23:37:04 -0800

    Giant goals going forward grows gun powder in guts pepared for gentle gastrastrophy

--- client/index.js
+++ client/index.js
@@ -12,11 +12,23 @@ window.onload = function (){
     {x: 60, y: 120},
     {x: 25, y: 23}
   ]
+
   const height = window.innerHeight,
         width = window.innerWidth
         line = d3.line(),
-        axis = d3.axisLeft()
-        x = d3.scaleOrdinal().range([0, width], 5)
+        axis = d3.axisLeft(),
+        x = d3.scaleBand().range([0, width])
+        //.domain(['async', 'bluebird', 'webpack', 'request'])
+        // .round([0.1])
+        .paddingInner([0.1])
+  	    .paddingOuter([0.3])
+  	    .align([0.5])
+        y = d3.scaleLinear()
+          .domain([0, 1])
+          .range([0, height])
+
+
+
 
 
   d3.json('/data.json', function(err, json) {
@@ -39,16 +51,20 @@ window.onload = function (){
               height: height
           });
 
+      //console.log( x.domain(['async', 'bluebird', 'webpack', 'request']))
+
       const g = svg.append('g')
           .attr('transform', 'translate(' + [10, 10] + ')' )
 
 
       const path = d3.line()
           .x(function(d) {
-            console.log(x(d.final))
-            return d.final * 20; })
+
+
+            return d.final * 200; })
           .y(function(d) {
-            return d.maintenance * 20; });
+            console.log(d.maintenance)
+            return y(d.maintenance); });
 
       const paths = g.append('g')
         .attr('class', 'score')
@@ -56,6 +72,17 @@ window.onload = function (){
         .data(i)
         .enter().append('path')
         .attr('d', path(i))
+
+        g.append("g")
+              .attr("class", "axis")
+              .each(function(d) { d3.select(this).call(axis.scale(x.domain(d3.extent(i, function(d){ return d.name})))); })
+
+
+                //so each axis has the linear scale for scores
+                //but I need an axis created for each datum
+                //look up rangepoints for spacing axes
+
+
 
     // g.append(g)
     //   .attr('class', 'axis')
