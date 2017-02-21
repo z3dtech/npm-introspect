d75aba9a2ebe3ed3e938610b32ef66388ef85075
nohmapp <nohman20@gmail.com>
2017-02-20 22:25:46 -0800

    Nothing normal, need nurishemnt, naked, nobby, hobbled, and neogeometrical, nothing works

--- client/index.js
+++ client/index.js
@@ -5,18 +5,30 @@ window.onload = function (){
   //when transform use two different objects to store
   //one for the score and one for the
 
-  // const data =[
-  // //  {name: ['async', 'bluebird', 'webpack', 'request']},
-  //   {final: [.84071299, .964345, .9622, .85729]},
-  //   {maintenance: [.66663, .75394, .96475, .4392]},
-  //   {popularity: [.96706, .85683, .89832, .8952]}
-  // ]
+  const datum =[
+  //{name: ['async', 'bluebird', 'webpack', 'request']},
+    .8888, .46464, .66663, .96706, .84071299, .86868, .63636, .96969
+  ]
 
   const coors = [
     {x: 45, y: 120},
     {x: 60, y: 120},
     {x: 25, y: 23}
   ]
+
+
+  /*
+6 by 4 = 36 calls
+3 by 4 = 9 calls
+4 by 5 = 16 calls
+1 by 5 = 25 calls
+
+
+
+
+
+
+  */
 
   const margin = {top: 100, right: 10, bottom: 100, left: 10},
         width = window.innerHeight - margin.left - margin.right,
@@ -37,7 +49,7 @@ d3.request(url)
     if (err) console.log(err)
 
     const data = JSON.parse(rawData)
-    console.log(data)
+    //console.log(data)
 
   // var i = []
   // json.forEach(function(d){
@@ -76,6 +88,8 @@ d3.request(url)
       const g = svg.append('g')
           .attr('transform', 'translate(' + [100, 10] + ')' )
 
+
+
           //this guy has to seperate the paths functio better
           //its all getting fucked up by the return statements
 
@@ -90,18 +104,39 @@ d3.request(url)
             //}).call()
       //  }
 
-      const path = function(data, value){
-        const gh = d3.line()
-         .x(function(d, i) {
+      // const path = function(data, value){
+      //   const gh = d3.line()
+      //   .x(function(d, i) {
+      //     return 100 * i; })
+      //   .y(function(d, i) {
+      //     //console.log(keys[i])
+      //     return y(d.value)
+      //   })
+      //    return gh(data)
+      //
+      // }
+
+      console.log('no error so far')
+
+
+      const path = d3.line()
+        .x(function(d, i) {
            return 100 * i; })
-         .y(function(d, i) {
+        .y(function(d, i) {
            //console.log(keys[i])
-           return y(d.value)
+           console.log(i)
+           console.log(d)
+           return y(.5)    //here is
          })
-         return gh(data)
 
-      }
 
+      //  const path2 = d3.line()
+      //    .x(function(d, i) {
+      //       return 100 * i; })
+      //    .y(function(d, i) {
+      //       //console.log(keys[i])
+      //       return y(d.popularity)
+      //     })
 
       // const path = d3.line()
       //     .x(function(d) {
@@ -109,24 +144,84 @@ d3.request(url)
       //     .y(function(d) {
       //       return y(d.maintenanc); });
 
+      //this should be wraqpped and called more itmes
+
+
       const paths = g.append('g')
         .attr('class', 'score')
-        .selectAll('path')
+        .selectAll('paths')
         .data(data)
+        .attr('d', data.map((d) => {
+             d.score.detail['final'] = d.score.final
+        }))
         .enter()
-        .attr('d', ()=> {
-          let keys = ['score.detail.maintenance', 'score.detail.popularity', 'score.detail.quality', 'score.final']
-          for(let i = 0; i < keys.length; i++){
-              path(data, keys[i]);
-          }
+        // .each((d, i) => {
+        //   console.log(d.score.detail)
+        //   console.log(i + ' ---------')
+        // })
+        .append('g')
+        .attr('transform', 'translate(' + [0, 0] + ')')
+        .attr('d', (d) => {
+            d.score.detail.each(d, i){
+              console.log(i)
+            }
+          //return path()
+
         })
 
 
 
+        // const paths = g.selectAll('paths')
+        //
+        //   .attr('class', 'lines')
+        //   .data(data)
+        //   .enter()
 
 
+          //.append('g')
+          //.attr('transform', 'translate(' + [10, 10] + ')')
+
+          //.selectAll('paths')
+
+
+
+        //   .selectAll('path')
+        //   .attr('d', data.map((d) => {
+        //     d.score.detail['final'] = d.score.final
+        //   })
+        // )
+        //   .data(function(d){
+        //     console.log(d.score.detail)
+        //     return d.score.detail
+        //   })
+        //   //.enter()
+        //   .append('g')
+        //   .attr('d', (d) =>{
+        //     console.log(d)
+        //   })
+        //   .append('path')
+        //   .attr('d', () =>{
+        //     console.log('here')
+        //     console.log(d)
+        //     return path(d)
+        //   })
+          // .selectAll('paths')
+          // .data(function(d){
+          //   return d.score.detail
+          // })
+          // .attr(function(d){
+          //   console.log(d)
+          // })
+
+
+
+        // ()=> {
+        //   let keys = ['score.detail.maintenance', 'score.detail.popularity', 'score.detail.quality', 'score.final']
+        //   for(let i = 0; i < keys.length; i++){
+        //       path(data, keys[i]);
+        //   }
         // .attrs({
-        //   'transform': 'translate(' + [0,0] + ')',
+        //
         //   fill: none,,
         //   stroke: steelblue,
         //   'stroke-width': '3px',
